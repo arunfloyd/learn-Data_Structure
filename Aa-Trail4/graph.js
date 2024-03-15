36988f085e476bf4fb6eaf266c0e5189fc3b60ca
Arun Vinod K <arunvinod9497@gmail.com>
2024-03-15 21:48:23 +0530

    Some Change

---
+++ Aa-Trail4/graph.js
@@ -0,0 +1,97 @@
+class Graph {
+    constructor() {
+      this.adjacencyList = {};
+    }
+    addVertex(vertex) {
+      if (!this.adjacencyList[vertex]) {
+        this.adjacencyList[vertex] = new Set();
+      }
+    }
+    addEdge(vertex1, vertex2) {
+      if (!this.adjacencyList[vertex1]) {
+        this.addVertex(vertex1);
+      }
+      if (!this.adjacencyList[vertex2]) {
+        this.addVertex(vertex2);
+      }
+      this.adjacencyList[vertex1].add(vertex2);
+      this.adjacencyList[vertex2].add(vertex1);
+    }
+    removeEdge(vertex1, vertex2) {
+      this.adjacencyList[vertex1].delete(vertex2);
+      this.adjacencyList[vertex2].delete(vertex1);
+    }
+    removeVertex(vertex) {
+      if (!this.adjacencyList[vertex]) {
+        return;
+      }
+      for (let adjacentVertex of this.adjacencyList[vertex]) {
+        this.removeEdge(vertex, adjacentVertex);
+      }
+      delete this.adjacencyList[vertex];
+    }
+    hasEdge(vertex1, vertex2) {
+      return (
+        this.adjacencyList[vertex1].has(vertex2) &&
+        this.adjacencyList[vertex2].has(vertex1)
+      );
+    }
+    display() {
+      for (let vertex in this.adjacencyList) {
+        console.log(vertex + "->" + [...this.adjacencyList[vertex]]);
+      }
+    }
+    bfs(startVertex) {
+      const visited = new Set();
+      const queue = [startVertex];
+  
+      while (queue.length > 0) {
+        const currentVertex = queue.shift();
+        if (!visited.has(currentVertex)) {
+          visited.add(currentVertex);
+          console.log(currentVertex);
+  
+          for (let neighbor of this.adjacencyList[currentVertex]) {
+            if (!visited.has(neighbor)) {   
+              queue.push(neighbor);
+            }
+          }
+        }
+      }
+    }
+    dfs(startVertex) {
+      const visited = new Set();
+  
+      const dfsTraversal = (vertex) => {
+        if (!visited.has(vertex)) {
+          visited.add(vertex);
+          console.log(vertex);
+          for (let neighbor of this.adjacencyList[vertex]) {
+            dfsTraversal(neighbor);
+          }
+        }
+      };
+  
+      dfsTraversal(startVertex);
+    }
+  }
+  
+  const graph = new Graph();
+  graph.addVertex("A");
+  graph.addVertex("B"); 
+  graph.addVertex("C");
+  
+  graph.addEdge("A", "B");
+  graph.addEdge("B", "C");
+  graph.display();
+  console.log(graph.hasEdge("A", "C"));
+  // graph.removeEdge("A", "B");
+  graph.removeVertex("B");
+  graph.display();
+  console.log(graph.bfs("A"));
+  console.log(graph.dfs("e"));
+  
+  //Constant Time Complexity O(1)
+  
+  //But on the removal of vetex takes the number of adjacent vertixes
+  
---
+++ Aa-Trail4/heapMin.js
@@ -0,0 +1,121 @@
+// Implement a min heap:
+
+// -> insert, extract_min
+
+// property:
+// - elements are in ascending order
+// - complete binary tree (node is smaller than it’s children)
+// - root is the most minimum
+// - insert takes O(logn) time
+//     - insert to the bottom right
+//     - bubble up until it meets requirements
+// - extract_min takes O(logn) time
+//     - replace min with bottom right
+//     - bubble down until it meets requirements
+
+class MinHeap {
+  constructor() {
+    this.data = [];
+  }
+  getParentIndex(i) {
+    return Math.floor((i - 1) / 2);
+  }
+  getLeftChildIndex(i) {
+    return i * 2 + 1;
+  }
+  getRightIndex(i) {
+    return i * 2 + 2;
+  }
+  swap(i1, i2) {
+    const temp = this.data[i1];
+    this.data[i1] = this.data[i2];
+    this.data[i2] = temp;
+  }
+  push(key) {
+    this.data.push(key); // Add the key to the end of the array
+    this.heapifyUp();
+  }
+  pop() {
+    const minValue = this.data[0];
+    this.data[0] = this.data[this.data.length - 1]; // Replace the root with the last element
+    this.data.pop(); // Remove the last element
+    this.heapifyDown();
+    return minValue;
+  }
+  heapifyDown() {
+    let currentIndex = 0;
+
+    while (this.getLeftChildIndex(currentIndex) < this.data.length) {
+      let smallestChildIndex = this.getLeftChildIndex(currentIndex);
+
+      if (
+        this.getRightIndex(currentIndex) < this.data.length &&
+        this.data[this.getRightIndex(currentIndex)] <
+          this.data[smallestChildIndex]
+      ) {
+        smallestChildIndex = this.getRightIndex(currentIndex);
+      }
+      if (this.data[currentIndex] > this.data[smallestChildIndex]) {
+        this.swap(currentIndex, smallestChildIndex);
+        currentIndex = smallestChildIndex;
+      } else {
+        return;
+      }
+    }
+  }
+  heapifyUp() {
+    let currentIndex = this.data.length - 1;
+    while (
+      currentIndex > 0 &&
+      this.data[currentIndex] < this.data[this.getParentIndex(currentIndex)]
+    ) {
+      this.swap(currentIndex, this.getParentIndex(currentIndex));
+      currentIndex = this.getParentIndex(currentIndex);
+    }
+  }
+  poll() {
+    const maxValue = this.data[0];
+    this.data[0] = this.data[this.data.length - 1];
+    this.data.length--;
+    this.heapifyDown();
+    return maxValue;
+  }
+}
+
+const minHeap = new MinHeap();
+minHeap.push(3);
+minHeap.push(23);
+minHeap.push(21);
+minHeap.push(13);
+minHeap.push(223);
+minHeap.push(121);
+console.log(minHeap.data.join(",")); // [3, 21, 23]
+minHeap.pop();
+console.log(minHeap.data.join(",")); // [21, 23]
+
+console.log("............................................");
+a = [];
+
+a.push(minHeap.poll());
+a.push(minHeap.poll());
+a.push(minHeap.poll());
+a.push(minHeap.poll());
+a.push(minHeap.poll());
+
+console.log("top 5 items", a);
+function heapSort(arr) {
+  const heap = new minHeap();
+  for (let i = 0; i < arr.length; i++) {
+    heap.push(arr[i]); // Use push instead of insert
+  }
+  const sorted = [];
+  while (heap.data.length > 0) {
+    // Use heap.data.length to check if the heap is empty
+    sorted.push(heap.poll()); // Use poll to remove and return the maximum element
+  }
+  return sorted;
+}
+
+let arr = [1, 6, 2, 3, 7, 3, 4, 6, 9];
+arr = heapSort(arr);
+console.log(arr);
